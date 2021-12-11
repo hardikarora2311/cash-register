b47c672a501c778f47e45ff56ca29d0ccf4f96ae
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2021-12-11 16:20:21 +0530

    further process

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ let errorMsg= document.querySelector(".error-msg");
 const returnChange= document.querySelector(".return");
 const noOfNotes= document.querySelectorAll(".no-of-notes");
 
+let showBill = document.querySelector("#bill");
+let showCash= document.querySelector("#cash");
+
 const notes= [2000,500,100, 50, 20, 10,5,1];
 
 checkBtn.addEventListener("click", ()=> {
     errorMsg.style.display="none"
     returnChange.innerHTML= "";
+    showCash.innerHTML= cashGiven.value;
+    showBill.innerHTML= billAmount.value;
     if (parseInt(billAmount.value) > 0){
         if (parseInt(cashGiven.value) >= parseInt(billAmount.value)){
             const returnAmnt= cashGiven.value- billAmount.value;
