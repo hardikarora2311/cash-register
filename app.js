8855d0e249c0d4cef237733e6261ede08993d3d2
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2021-12-11 16:14:34 +0530

    processing done

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 let billAmount= document.querySelector("#bill-ammount");
 let checkBtn= document.querySelector(".check-btn");
-let cashGiven= document.querySelector(".cash-given");
+let cashGiven= document.querySelector("#cash-given");
+let errorMsg= document.querySelector(".error-msg");
+const returnChange= document.querySelector(".return");
+const noOfNotes= document.querySelectorAll(".no-of-notes");
+
+const notes= [2000,500,100, 50, 20, 10,5,1];
 
 checkBtn.addEventListener("click", ()=> {
-    if (billAmount.value > 0){
-        
+    errorMsg.style.display="none"
+    returnChange.innerHTML= "";
+    if (parseInt(billAmount.value) > 0){
+        if (parseInt(cashGiven.value) >= parseInt(billAmount.value)){
+            const returnAmnt= cashGiven.value- billAmount.value;
+            printChange(returnAmnt);
+        }else{
+            showError("Given cash is less than the bill amount.");
+        }
+    }else{
+        showError("Enter valid bill amount ie. greater than 0.");
     }
 })
+
+function showError(msg){
+    errorMsg.style.display="block";
+    errorMsg.innerHTML= msg;
+    console.log("error");
+}
+
+function printChange(amnt){
+    returnChange.innerHTML= amnt;
+    for (let i=0; i< notes.length; i++){
+        const numberOfNotes=  Math.trunc(amnt/notes[i]);
+        amnt %= notes[i];
+        noOfNotes[i].innerText= numberOfNotes;
+    }
+}
 
 
