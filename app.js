752dae5149eae52cf853ac7f09b9c3fdce37ef67
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2021-12-11 10:50:02 +0530

    adding js

---
+++ app.js
@@ -0,0 +1,11 @@
+let billAmount= document.querySelector("#bill-ammount");
+let checkBtn= document.querySelector(".check-btn");
+let cashGiven= document.querySelector(".cash-given");
+
+checkBtn.addEventListener("click", ()=> {
+    if (billAmount.value > 0){
+        
+    }
+})
+
+
